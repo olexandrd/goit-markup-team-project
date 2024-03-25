4069a4f2b89e0a1807008103e1baa99b38042c46
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-03-25 22:16:23 +0200

    add close menu on cllick anchor link

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
+const menuLinks = document.querySelectorAll('.mobile-menu-link');
 const toggleMenu = () => {
   const isMenuOpen =
     openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -13,6 +14,11 @@ const toggleMenu = () => {
 };
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    toggleMenu();
+  });
+});
 // Close the mobile menu on wider screens if the device orientation changes
 window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
   if (!e.matches) return;
